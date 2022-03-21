1df99a8263d237fc099bd61242031e345669192d
stanfrbd <44167150+stanfrbd@users.noreply.github.com>
2022-03-21 14:59:30 +0100

    Update install_vagrant.sh

--- install_vagrant.sh
+++ install_vagrant.sh
@@ -2,10 +2,12 @@
 
 # must be run as root
 
-apt-get update
+apt-get update -y
 apt-get install -y qemu-kvm libvirt-daemon-system libvirt-dev
 # apt-get install -y linux-image-$(uname -r)
 apt-get install -y curl net-tools jq
+
+apt-get autoremove
 
 curl -O https://releases.hashicorp.com/vagrant/$(curl -s https://checkpoint-api.hashicorp.com/v1/check/vagrant  | jq -r -M '.current_version')/vagrant_$(curl -s https://checkpoint-api.hashicorp.com/v1/check/vagrant  | jq -r -M '.current_version')_x86_64.deb
 dpkg -i vagrant_$(curl -s https://checkpoint-api.hashicorp.com/v1/check/vagrant  | jq -r -M '.current_version')_x86_64.deb
